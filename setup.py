928d4867525092a821e000be9820b2897a67ade6
Matthew Conlin <32144021+conlin-matt@users.noreply.github.com>
2021-06-10 09:47:32 -0400

    Make version OG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pyCLARIS",
-    version="0.1.2",
+    version="0.0.1",
     url="https://github.com/conlin-matt/pyCLARIS",
     author="Matthew P. Conlin",
     author_email="conlinm@ufl.edu",
